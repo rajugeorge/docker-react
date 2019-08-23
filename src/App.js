251ddee0e4e3ef520393329046e3536535eef878
Raju George <rajugeorge@gmail.com>
2019-08-23 13:09:40 +0530

    local commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This has to be changed for once and for all.
         </p>
         <a
           className="App-link"
